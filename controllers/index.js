c39d4ed00aa419faf8faab23d3217b11a07f2d41
kraigmansfield <Kraig.mansfield@gmail.com>
2023-02-03 15:12:51 -0900

    added post

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,4 @@
+const { notStrictEqual } = require("assert");
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
@@ -12,6 +13,18 @@ router.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '../public/notes.html'))
 );
 
+router.post('/notes', (req,res) => {
+  console.log(req.body)
+  const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+  const newNotes = req.body
+  notes.push(newNotes);
+  fs.writeFileSync("./db/db.json", JSON.stringify(notes))
+  res.json(notes);
+  res.json({msg:"connected"})
+})
 
+router.delete('/:notes',(req,res)=>{
+
+})
 
 module.exports = router;
